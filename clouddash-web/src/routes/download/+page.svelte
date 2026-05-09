2848743a3e3de4f61f5db957862211300463da32
Hao <hao1300@gmail.com>
2026-05-09 10:31:08 -0500

    Update windows

--- clouddash-web/src/routes/download/+page.svelte
+++ clouddash-web/src/routes/download/+page.svelte
@@ -15,7 +15,7 @@
       name: "Windows",
       icon: MonitorIcon,
       type: "Desktop App",
-      link: "https://static.clouddash.dev/downloads/windows/clouddash-0.9.6-setup.exe",
+      link: "https://static.clouddash.dev/downloads/windows/clouddash-1.0.0-setup.exe",
       instructions:
         "Stable installer for Windows 10/11. Includes auto-updates and system tray support.",
     },
